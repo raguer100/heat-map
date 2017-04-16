6508c6616ed8d241e3de6eda9ac6e4da724f718b
Hilmi ARAZ <hilmi_58_araz@hotmail.com>
2017-04-16 18:15:00 +0300

    1.0.0.1
    Mount/Week implemented

--- client/App.jsx
+++ client/App.jsx
@@ -8,6 +8,16 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+
+    var parser = function(data) {
+    	var stats = {};
+    	for (var d in data) {
+    		stats[new Date(data[d].baslangic).getTime() / 1000] = data[d].toplamilerleme;
+    	}
+
+	return stats;
+};
+
     var cal = new CalHeatMap();
     /*cal.init({
     legendColors: {
@@ -48,14 +58,36 @@ export default class App extends Component {
         width: 46,
         rotate: "null"
       },
-      previousSelector: document.getElementsByClassName("fa fa-angle-left fa-3x")[0],
-      nextSelector: document.getElementsByClassName("fa fa-angle-right fa-3x")[0],
+      previousSelector: document.getElementsByClassName("fa fa-angle-left fa-3x 1")[0],
+      nextSelector: document.getElementsByClassName("fa fa-angle-right fa-3x 2")[0],
       start: new Date(),
       legend: [200, 350, 500, 650, 800],
       displayLegend: true,
       legendVerticalPosition: "bottom",
       legendHorizontalPosition: "center",
     });
+
+    var cal1 = new CalHeatMap();
+    cal1.init({
+      itemSelector: document.getElementsByClassName("cal1")[0],
+      domain: "month",
+      subDomain: "week",
+      subDomainTextFormat: "%d-%W",
+      domainLabelFormat: "%b %Y",
+      cellSize: 40,
+      domainMargin: 10,
+      domainGutter: 10,
+      weekStartOnMonday: true,
+      afterLoadData: parser,
+      range: 4,
+      displayLegend: false,
+      previousSelector: document.getElementsByClassName("fa fa-angle-left fa-3x")[0],
+      nextSelector: document.getElementsByClassName("fa fa-angle-right fa-3x")[0],
+      start: new Date(),
+    });
+
+
+
 
     /*cal.init({
     itemSelector: document.getElementsByClassName("cal")[0],
@@ -77,10 +109,12 @@ export default class App extends Component {
   render() {
     return(
       <div>
-        <div className="cal"></div>
-
+        <div className="cal1"></div>
         <i className="fa fa-angle-left fa-3x" aria-hidden="true"></i>
         <i className="fa fa-angle-right fa-3x" aria-hidden="true"></i>
+        <div className="cal"></div>
+        <i className="fa fa-angle-left fa-3x 1" aria-hidden="true"></i>
+        <i className="fa fa-angle-right fa-3x 2" aria-hidden="true"></i>
       </div>
     );
   }
